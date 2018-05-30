2447e0d7e6da82bd6d933a8f954bb9f171a2f15e
Haylee Fay Ham <hayleeham@gmail.com>
2018-05-29 22:56:45 -0500

    fixing broken note dictionary and a little styling

--- app.py
+++ app.py
@@ -2,24 +2,35 @@ from flask import Flask, render_template, request
 import requests
 from bs4 import BeautifulSoup
 from itertools import chain
+import collections
 
 app = Flask(__name__)
 
-NOTE_FREQ = {'C#_1': 277.18, 'D#_1': 207.65, 'F#_1': 369.99, 'G#_1': 415.30,
-             'A#_1': 466.16, 'C#_2': 554.37, 'D#_2': 622.25, 'F#_2': 739.99,
-             'G#_2': 830.61, 'A#_2': 932.33, 'G_1': 196.00, 'A_1': 220.00,
-             'B_1': 246.94, 'C_1': 261.63, 'D_1': 293.66, 'E_1': 329.63,
-             'F_1': 349.23, 'G_2': 392.00, 'A_2': 440.00, 'B_2': 493.88,
-             'C_2': 523.25, 'D_2': 587.33, 'E_2': 659.25, 'F_2': 698.46,
-             'G_3': 783.99, 'A_3': 880.00, 'B_3': 987.77, 'C_3': 1046.50}
+# NOTE_FREQ = {'C#_1': 277.18, 'D#_1': 207.65, 'F#_1': 369.99, 'G#_1': 415.30,
+#              'A#_1': 466.16, 'C#_2': 554.37, 'D#_2': 622.25, 'F#_2': 739.99,
+#              'G#_2': 830.61, 'A#_2': 932.33, 'G_1': 196.00, 'A_1': 220.00,
+#              'B_1': 246.94, 'C_1': 261.63, 'D_1': 293.66, 'E_1': 329.63,
+#              'F_1': 349.23, 'G_2': 392.00, 'A_2': 440.00, 'B_2': 493.88,
+#              'C_2': 523.25, 'D_2': 587.33, 'E_2': 659.25, 'F_2': 698.46,
+#              'G_3': 783.99, 'A_3': 880.00, 'B_3': 987.77, 'C_3': 1046.50}
 
-SHEET_NOTES = {207.65: 'D', 369.99: 'F', 415.30: 'G', 466.16: 'A',
-               554.37: 'C', 622.25: 'D', 739.99: 'f', 830.61: 'g',
-               932.33: 'a', 196.00: 'G', 220.00: 'A', 246.94: 'B',
-               261.63: 'C', 293.66: 'D', 329.63: 'E', 349.23: 'F',
-               392.00: 'g', 440.00: 'a', 493.88: 'b', 523.25: 'c',
-               587.33: 'd', 659.25: 'e', 698.46: 'f', 783.99: "g",
-               880.00: "a", 987.77: "b", 1046.50: "c", 277.18: 'C'}
+NOTE_FREQ = {'G_1': 196.00, 'G#_1': 207.65, 'A_1': 220.00, 'A#_1': 233.08,
+             'B_1': 246.94, 'C_1': 261.63, 'C#_1': 277.18, 'D_1': 293.66,
+             'D#_1': 311.13, 'E_1': 329.63, 'F_1': 349.23, 'F#_1': 369.99,
+             'G_2': 392.00, 'G#_2': 415.30, 'A_2': 440.00, 'A#_2': 466.16,
+             'B_2': 493.88, 'C_2': 523.25, 'C#_2': 554.37, 'D_2': 587.33,
+             'D#_2': 622.25, 'E_2': 659.25, 'F_2': 698.46, 'F#_2': 739.99,
+             'G_3': 783.99, 'G#_3': 830.61, 'A_3': 880.00, 'A#_3': 932.33,
+             'B_3': 987.77, 'C_3': 1046.50}
+
+SHEET_NOTES = {196.00: 'G,', 207.65: 'G,', 220.00: 'A,', 233.08: 'A,',
+               246.94: 'B,', 261.63: 'C,', 277.18: 'C,', 293.66: 'D,',
+               311.13: 'D,', 329.63: 'E,', 349.23: 'F,', 269.99: 'F,',
+               392.00: 'G', 415.30: 'G', 440.00: 'A', 466.16: 'A',
+               493.88: 'B', 523.25: 'C', 554.37: 'C', 587.33: 'D',
+               622.25: 'D', 659.25: 'E', 698.46: 'F', 739.99: 'F',
+               783.99: 'g', 830.61: 'g', 880.00: 'a', 932.33: 'a',
+               987.77: 'b', 1046.50: 'c'}
 
 NOTE_LIST = list(NOTE_FREQ.keys())
 
@@ -29,7 +40,7 @@ D_MAJOR = ['G_1', 'A_1', 'B_1', 'C#_1', 'D_1', 'E_1', 'F#_1', 'G_2', 'A_2', 'B_2
 
 KEYS = {'Cmaj': C_MAJOR, 'Gmaj': G_MAJOR, 'Dmaj': D_MAJOR}
 KEY_LIST = ['Cmaj', 'Gmaj', 'Dmaj']
-TEMPOS = [70, 120, 250, 600]
+TEMPOS = [120, 250, 400, 600]
 DURATIONS = [1, 2, 4, 8]
 
 
@@ -94,6 +105,20 @@ def map_protein(protein, regions):
 
     return key, tempo, notes, duration
 
+    '''
+    # doing tempo based off of amino acid distribution
+    dist = collections.Counter(protein)
+
+
+    print(dist)
+
+
+    for i, x in protein:
+
+
+    return key, tempo, notes, duration
+    '''
+
 
 def color_domain(sequence, coding_regions):
     colored_sequence = ''
@@ -122,10 +147,7 @@ def create_sheet(notes, durations):
 
         total_duration += durations[index]
 
-        if total_duration % 8 == 0 and total_duration != 0:
-            sheet_string += '|'
-
-        if total_duration % 16 == 0:
+        if index % 16 == 0 and index != 0:
             sheet_string += '\n'
 
     sheet_string += "|]"
@@ -144,8 +166,16 @@ def create_coding_regions(xml_text):
                 region = k.GBFeature_key.find_next('GBFeature_location').contents
                 i = region[0].find('..')
 
-                start = int(region[0][:i].strip('<'))
-                stop = int(region[0][i+2:].strip('<'))
+                if '<' in region[0]:
+                    start = int(region[0][:i].strip('<'))
+                    stop = int(region[0][i+2:].strip('<'))
+                elif '>' in region[0]:
+                    start = int(region[0][:i].strip('>'))
+                    stop = int(region[0][i+2:].strip('>'))
+                else:
+                    start = int(region[0][:i])
+                    stop = int(region[0][i+2:])
+
                 coding_regions.append([start, stop])
 
     return coding_regions
@@ -174,7 +204,12 @@ def make_song():
 
         colored_sequence = color_domain(sequence, coding_regions)
 
-        return render_template('song.html', protein_seq=colored_sequence, protein_name=name, notes=notes, durations=durations, tempo=tempo, coding_regions=coding_regions, sheet_string=sheet_string, key=key)
+        sheet_name = name if len(name) <= 60 else name[:60] + '...'
+
+        return render_template('song.html', protein_seq=colored_sequence,
+                               protein_name=name, notes=notes, durations=durations,
+                               tempo=tempo, coding_regions=coding_regions,
+                               sheet_string=sheet_string, key=key, sheet_name=sheet_name)
 
     error = 'sorry'
     return render_template('error.html', error=error)
